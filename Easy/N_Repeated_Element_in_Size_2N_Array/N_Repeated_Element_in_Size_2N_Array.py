9c1e79a1e424ced4e80aa3e790a9c17f9e496b62
NithinShekhar <nitin3685@gmail.com>
2019-12-10 11:01:11 +0530

    Update N_Repeated_Element_in_Size_2N_Array.py

--- Easy/N_Repeated_Element_in_Size_2N_Array/N_Repeated_Element_in_Size_2N_Array.py
+++ Easy/N_Repeated_Element_in_Size_2N_Array/N_Repeated_Element_in_Size_2N_Array.py
@@ -1,6 +1,7 @@
 class Solution:
     def repeatedNTimes(self, A: List[int]) -> int:
         dict_A = {}
+        #Use set to make it faster
         for index,num in enumerate(A):
             if num not in dict_A.keys():
                 dict_A[num] = index
