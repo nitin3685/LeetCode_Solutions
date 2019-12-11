2f6d340bd117e0a2bb0438a938719f280b3b95cf
NithinShekhar <nitin3685@gmail.com>
2019-12-11 14:21:46 +0530

    Update Array_Partition_I_slicing.py

--- Easy/Array_Partition_I/Array_Partition_I_slicing.py
+++ Easy/Array_Partition_I/Array_Partition_I_slicing.py
@@ -1,4 +1,7 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-            return sum(sorted(nums)[::2])
+        return sum(sorted(nums)[::2])
+        #faster    
+        #nums.sort()
+        #return sum(nums[::2])
         
