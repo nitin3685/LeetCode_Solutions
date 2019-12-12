5743379089dd7d2fcfcc18201b57d5e1d05cc2d5
NithinShekhar <nitin3685@gmail.com>
2019-12-12 15:39:28 +0530

    Create N-ary_Tree_Level_Order_Traversal_Dqueue.py

---
+++ Medium/N-ary_Tree_Level_Order_Traversal/N-ary_Tree_Level_Order_Traversal_Dqueue.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return None
+        if not root.children:
+            return [[root.val]]
+        
+        res = []
+        queue = collections.deque([root])
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                queue.extend(node.children)
+            res.append(level)
+        return res
